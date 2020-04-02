c3b179fa3b603aafa6aecd4555f3f97a3647d03d
Drew Barton <60532893+Drev917@users.noreply.github.com>
2020-04-02 11:53:31 -0600

    Update GamblersRuin.py

--- GamblersRuin.py
+++ GamblersRuin.py
@@ -1,33 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-#Gambler's Ruin
-
 import random
+import matplotlib.pyplot as plt
 
-#while loop adds $1 to balance for every 1 int and subtracts $1 for ever 2 int
+#While loop adds $1 to balance for every 1 int and subtracts $1 for ever 2 int
 #Simulation is run 100 times and prints coin flips performed
 
-listofNumberFlips = []
+balance = 5
+balanceHist = []
+coinFlips = 0
+coinFlipsHist = []
 for trialNumber in range(100):
-    balance = 3
-    coinFlips = 0
     while(balance > 0):
         flipResult = random.randint(1,2)
         if(flipResult == 1):
             balance += 1
+            balanceHist.append(balance)
         else:
             balance -= 1
+            balanceHist.append(balance)
         coinFlips += 1
-    print('Total simulations run: ' + str(trialNumber))
-    print('Toal number of coin flips: ' + str(coinFlips))
-    listofNumberFlips.append(coinFlips)
+        coinFlipsHist.append(coinFlips)
+            
+    
+print('Toal number of coin flips: ' + str(coinFlips))
 
-"""
-Max number of coin flips overserved: 2043
-Minimum number of coin flips observed: 3
-"""
+plt.scatter(coinFlipsHist, balanceHist)
+plt.title('Distribution of Coinflips vs Balance')
+plt.ylabel('Balance')
+plt.xlabel('Coinflips') 
